3bff4f73c427f68bf32a1fc147be312f822e5ccc
Hassan <aeae9992>
2018-10-09 00:28:05 +0200

    Small Update 1.15

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,21 @@
 // CountTo JS Function
-$('.timer').countTo();
+$('#Numbers').on('inview', function (event, visible) {
+    if (visible == true) {
+        $('.timer').countTo('restart');
+    }
+});
 // PreLoader Function
 window.onload = function(){
-    this.document.querySelector('.loader').style.display = 'none';
+    document.querySelector('.loader').style.display = 'none';
+};
+// Contact Me Animation Function
+var loop = setInterval(animation , 5000);
+function animation(){
+    document.querySelector('.contact').style.animation = 'animate 0.55s ease';
+    document.querySelector('.message').style.animation = 'animate 0.55s ease 0.75s';
+    setTimeout(clear , 1500);
+    function clear(){
+    document.querySelector('.contact').style.animation = 'none';
+    document.querySelector('.message').style.animation = 'none';
+    }
 }
